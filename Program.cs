8c4d2b0ce36ecec4a5d5d9155cbec2d42034c85e
unknown <afiq.mustaffa@cloud.connect.asia>
2018-11-14 14:41:57 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace AkkaCluster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Console.WriteLine("Hello World!");
+            var config = ConfigurationFactory.ParseString(File.ReadAllText("config.hocon"));
+
+            ActorSystem actorSystem = ActorSystem.Create("akkacommerce",config);
+
+            Console.CancelKeyPress += (sender, eventArgs) => { };
+
+            actorSystem.WhenTerminated.Wait();
+
+        }
+    }
+}
